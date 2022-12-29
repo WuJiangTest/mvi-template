e547a02963c082293e140c995e26b6ba960aee29
GitHub Action <action@github.com>
2022-12-29 02:23:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/wujiangtest/mvitemplate/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/wujiangtest/mvitemplate/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.wujiangtest.mvitemplate.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.wujiangtest.mvitemplate.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
